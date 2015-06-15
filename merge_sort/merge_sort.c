59e446ae841690036063649f9be4b87b7181f541
Jeffrey Leung <j.leung51@shaw.ca>
2015-06-14 17:40:50 -0700

    Removing excess code in merge sort; tidying up documentation.

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -15,66 +15,70 @@
 #include "merge_sort.h"
 
 // Only for use by merge_sort().
-// This function merges two sorted arrays to create a single array (in the memory spaces of the
-// original arrays) with elements from least to greatest.
-// array1 and array2 must be in consecutive memory spaces - i.e. two sections of the same array.
+// This function merges two sorted arrays to create a single array (in the
+// memory spaces of the original arrays) with elements from least to greatest.
+// array1 and array2 must be in consecutive memory spaces - i.e. two sections
+// of the same array.
 static void merge_arrays( int* array1, int len1, int* array2, int len2 );
 
 
 
 // Only for use by merge_sort().
-// This function merges two sorted arrays to create a single array (in the memory spaces of the
-// original arrays) with elements from least to greatest.
-// array1 and array2 must be in consecutive memory spaces - i.e. two sections of the same array.
+// This function merges two sorted arrays to create a single array (in the
+// memory spaces of the original arrays) with elements from least to greatest.
+// array1 and array2 must be in consecutive memory spaces - i.e. two sections
+// of the same array.
 static void merge_arrays( int* array1, int len1, int* array2, int len2 )
 {
   int* array_final = array1;
-
   int len_final = len1 + len2;
   int array_copy[len_final];
 
-  int* array1temp = array1;
-  int* array2temp = array2;
-  unsigned int i = 0;
-
+  int i = 0;
   while( len1 > 0 && len2 > 0 )
   {
-    if( array1temp[0] < array2temp[0] )  // Insert element from array1 into array_copy
+    if( array1[0] < array2[0] )  // Insert element from array1 into array_copy
     {
-      array_copy[i] = array1temp[0];
-      array1temp += 1;
+      array_copy[i] = array1[0];
+      array1 += 1;
       len1--;
     }
     else  // Insert element from array2 into array_copy
     {
-      array_copy[i] = array2temp[0];
-      array2temp += 1;
+      array_copy[i] = array2[0];
+      array2 += 1;
       len2--;
     }
 
     i++;
   }
 
-  if( len1 > 0 )
+  // Leftover elements
+  int j;
+  for( j = 0; j < len1; ++j )
   {
-    memcpy( array_copy + i, array1temp, len1 * sizeof(int) );
+    array_copy[i + j] = array1[j];
   }
-  else if( len2 > 0 )
+  for( j = 0; j < len2; ++j )
   {
-    memcpy( array_copy + i, array2temp, len2 * sizeof(int) );
+    array_copy[i + j] = array2[j];
   }
 
-  memcpy( array_final, array_copy, len_final * sizeof(int) );
+  for( j = 0; j < len_final; ++j )
+  {
+    array_final[j] = array_copy[j];
+  }
 
   return;
 }
 
-// This function recursively sorts an array from least to greatest using the merge sort algorithm.
+// This function recursively sorts an array from least to greatest using the
+// merge sort algorithm.
 void merge_sort( int* array, int len )
 {
   if( array == NULL )
   {
-    printf( "Error: merge_sort was given a null pointer.\n" );
+    printf( "Error: merge_sort() was given a null pointer.\n" );
     exit( 1 );
   }
 
@@ -91,4 +95,4 @@ void merge_sort( int* array, int len )
   merge_arrays( array, mid, array+mid, len-mid );
 
   return;
-} 
+}
