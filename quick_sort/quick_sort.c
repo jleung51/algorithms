bffde94a932a3ae10e009d314a9fe3af3deb8840
Jeffrey Leung <j.leung51@shaw.ca>
2015-06-14 17:46:15 -0700

    Tidying up quick sort code and documentation.

--- quick_sort/quick_sort.c
+++ quick_sort/quick_sort.c
@@ -15,13 +15,13 @@
 
 // Only for use by quick_sort().
 // This function swaps the values of two integers, given their pointers.
-static void swap_qsort( int* a, int* b );
+static void swap_quick_sort( int* a, int* b );
 
 
 
 // Only for use by quick_sort().
 // This function swaps the values of two integers, given their pointers.
-static void swap_qsort( int* a, int* b )
+static void swap_quick_sort( int* a, int* b )
 {
   int temp = *a;
   *a = *b;
@@ -30,12 +30,13 @@ static void swap_qsort( int* a, int* b )
   return;
 }
 
-// This function recursively sorts an array from least to greatest using the quicksort algorithm.
+// This function recursively sorts an array from least to greatest using the
+// quicksort algorithm.
 void quick_sort( int* array, int len )
 {
   if( array == NULL )
   {
-    printf( "Error: quick_sort was given a null pointer.\n" );
+    printf( "Error: quick_sort() was given a null pointer.\n" );
     exit( 1 );
   }
 
@@ -44,30 +45,28 @@ void quick_sort( int* array, int len )
     return;
   }
 
-  const int PIVOT = 0;
   int mid = 0;
 
   // Loop invariants:
-  //   All elements between indices 1 and mid (inclusive) are less than or equal to array[PIVOT]
-  //   All elements between indices mid+1 and sweep-1 (inclusive) are greater than array[PIVOT]
-  //   All elements between indices sweep and len-1 (inclusive) are unknown
+  //   All elements between indices 1 and mid (inclusive) are less than or
+  //   equal to array[0].
+  //   All elements between indices mid+1 and sweep-1 (inclusive) are greater
+  //   than array[0].
+  //   All elements between indices sweep and len-1 (inclusive) are unknown.
   int sweep;
   for( sweep = 1; sweep < len; sweep++ )
   {
-    if( array[sweep] < array[PIVOT] )
+    if( array[sweep] < array[0] )
     {
-      swap_qsort( array + (mid+1), array + (sweep) );
+      swap_quick_sort( array + (mid+1), array + (sweep) );
       mid++;
     }
   }
 
-  swap_qsort( array, array + mid );
+  swap_quick_sort( array, array + mid );
 
-  int len_array_1 = mid;
-  int len_array_2 = len - (mid+1);
-
-  quick_sort( array,       len_array_1 );
-  quick_sort( array+mid+1, len_array_2 );
+  quick_sort( array,       mid );
+  quick_sort( array+mid+1, len - (mid+1) );
 
   return;
 }
