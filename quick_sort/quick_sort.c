cdadcfb2ff4b50d5f451b796415a3cc3cd053813
Jeffrey Leung <j.leung51@shaw.ca>
2015-06-24 19:44:22 -0700

    Adding randomization to avoid worst case scenario.

--- quick_sort/quick_sort.c
+++ quick_sort/quick_sort.c
@@ -1,7 +1,7 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-06-14
+ * Last edited: 2015-06-24
  *
  * This C program contains the implementation of a recursive quicksort
  * algorithm on an array of integers.
@@ -14,10 +14,52 @@
 #include "quick_sort.h"
 
 // Only for use by quick_sort().
+// This function randomizes the contents of an array.
+static void randomize_array_quick_sort( int* array, unsigned int len );
+
+// Only for use by quick_sort().
 // This function swaps the values of two integers, given their pointers.
 static void swap_quick_sort( int* a, int* b );
 
+// This function recursively sorts an array from least to greatest using the
+// quicksort algorithm.
+static void quick_sort_helper( int* array, unsigned int len );
 
+
+
+// Only for use by quick_sort().
+// This function randomizes the contents of an array.
+static void randomize_array_quick_sort( int* array, unsigned int len )
+{
+  if( array == NULL )
+  {
+    printf( "Implementation error: randomize_array_quicksort() was given a "\
+            "null pointer.\n" );
+    exit( 2 );
+  }
+
+  int array_random[len];
+  int len_temp = len;
+  int remove;
+
+  int i;
+  for( i = 0; i < len; ++i )
+  {
+    remove = rand() % len_temp;
+    array_random[i] = array[remove];
+
+    array[remove] = array[len_temp-1];
+    --len_temp;
+  }
+
+  int j;
+  for( j = 0; j < len; ++j )
+  {
+    array[j] = array_random[j];
+  }
+
+  return;
+}
 
 // Only for use by quick_sort().
 // This function swaps the values of two integers, given their pointers.
@@ -32,7 +74,7 @@ static void swap_quick_sort( int* a, int* b )
 
 // This function recursively sorts an array from least to greatest using the
 // quicksort algorithm.
-void quick_sort( int* array, unsigned int len )
+static void quick_sort_helper( int* array, unsigned int len )
 {
   if( array == NULL )
   {
@@ -65,8 +107,17 @@ void quick_sort( int* array, unsigned int len )
 
   swap_quick_sort( array, array + mid );
 
-  quick_sort( array,       mid );
-  quick_sort( array+mid+1, len - (mid+1) );
+  quick_sort_helper( array,       mid );
+  quick_sort_helper( array+mid+1, len - (mid+1) );
 
+  return;
+}
+
+// This function recursively sorts an array from least to greatest using the
+// quicksort algorithm, after randomizing the array to prevent the worst case.
+void quick_sort( int* array, unsigned int len )
+{
+  randomize_array_quick_sort( array, len );
+  quick_sort_helper( array, len );
   return;
 }
