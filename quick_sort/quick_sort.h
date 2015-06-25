c10c2f08e30db922deec8db0c48b96ba6af6426a
Jeffrey Leung <j.leung51@shaw.ca>
2015-06-24 19:44:38 -0700

    Updating documentation.

--- quick_sort/quick_sort.h
+++ quick_sort/quick_sort.h
@@ -1,7 +1,7 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2015-06-14
+ * Last edited: 2015-06-24
  *
  * This C program contains the implementation of a recursive quicksort
  * algorithm on an array of integers.
@@ -11,7 +11,8 @@
 #ifndef QUICK_SORT_H
 #define QUICK_SORT_H
 
-// This function recursively sorts an array from least to greatest using the quicksort algorithm.
+// This function recursively sorts an array from least to greatest using the
+// quicksort algorithm, after randomizing the array to prevent the worst case.
 void quick_sort( int* array, unsigned int len );
 
 #endif
