c33e06954c77ca0042ef88bf0f3c6bb9dd499e05
Jeffrey Leung <j.leung51@shaw.ca>
2015-06-25 22:11:22 -0700

    Adding tested, functional cocktail sort implementation.

--- cocktail_sort/cocktail_sort.c
+++ cocktail_sort/cocktail_sort.c
@@ -12,26 +12,47 @@
 // cocktail sort algorithm.
 void cocktail_sort( int* array, unsigned int len )
 {
-/*
-  // Bubble sort implementation
   int sorted_check = 1;
+  int forwards = 1;
   while( sorted_check )
   {
     sorted_check = 0;
 
-    int i;
-    for( i = 0; i < len-1; ++i )
+    if( forwards == 1 )
     {
-      if( array[i] > array[i+1] )
+      int i;
+      for( i = 0; i < len-1; ++i )
       {
-        int temp = array[i];
-        array[i] = array[i+1];
-        array[i+1] = temp;
+        if( array[i] > array[i+1] )
+        {
+          int temp = array[i];
+          array[i] = array[i+1];
+          array[i+1] = temp;
 
-        sorted_check = 1;
+          sorted_check = 1;
+        }
       }
+      forwards = 0;
     }
+
+    else
+    {
+      int i;
+      for( i = len-2; i > -1; --i )
+      {
+        if( array[i] > array[i+1] )
+        {
+          int temp = array[i];
+          array[i] = array[i+1];
+          array[i+1] = temp;
+
+          sorted_check = 1;
+        }
+      }
+      forwards = 1;
+    }
+
   }
-*/
+
   return;
 }
