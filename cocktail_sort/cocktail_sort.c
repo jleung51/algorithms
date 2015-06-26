4e5d7d89465a918f531ab9531b88ca592f7cf047
Jeffrey Leung <j.leung51@shaw.ca>
2015-06-25 21:11:47 -0700

    Adding skeleton files as wel as bubble sort implementation (for reference).

---
+++ cocktail_sort/cocktail_sort.c
@@ -0,0 +1,37 @@
+/*
+ *
+ * Author: Jeffrey Leung
+ * Last edited: 2015-06-25
+ *
+ * This C program contains the implementation of a cocktail sort algorithm on
+ * an array of intgers.
+ *
+ */
+
+// This function sorts an array from least to greatest using the
+// cocktail sort algorithm.
+void cocktail_sort( int* array, unsigned int len )
+{
+/*
+  // Bubble sort implementation
+  int sorted_check = 1;
+  while( sorted_check )
+  {
+    sorted_check = 0;
+
+    int i;
+    for( i = 0; i < len-1; ++i )
+    {
+      if( array[i] > array[i+1] )
+      {
+        int temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+
+        sorted_check = 1;
+      }
+    }
+  }
+*/
+  return;
+}
