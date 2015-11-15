9331fb6e07f6baaca385d5292abdde6ca333e36e
jleung51 <jleung513@gmail.com>
2015-11-14 23:44:48 -0800

    Sorts/Cpp: Adding auto-comparison against STL sort in testing.

--- sorts/other-languages/cpp/test/test.cpp
+++ sorts/other-languages/cpp/test/test.cpp
@@ -3,10 +3,12 @@
  * Author: Jeffrey Leung
  * Last edited: 2015-11-14
  *
- * This C++ file tests the implementation of the C++ sort functions.
+ * This C++ file tests the implementation of the C++ sort functions against
+ * the STL sort implementation.
  *
  */
 
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 #include <iterator>
@@ -22,6 +24,11 @@ namespace
 // structure in a horizontal list, surrounded by brackets [].
 template <class Iterator>
 void PrintHorizontal( const Iterator begin, const Iterator end );
+
+// This local function returns true if two given iterable data structures
+// contain equivalent values, and false otherwise.
+template <class Iterator>
+bool Equivalent( const Iterator begin, const Iterator end );
 
 // This local function prints the contents of a given iterable data
 // structure in a horizontal list, surrounded by brackets [].
@@ -39,17 +46,46 @@ void PrintHorizontal( const Iterator begin, const Iterator end )
   return;
 }
 
+// This local function returns true if two given iterable data structures
+// contain equivalent values, and false otherwise.
+template <class Iterator>
+bool Equivalent( const Iterator begin_1, const Iterator end_1,
+                 const Iterator begin_2, const Iterator end_2 )
+{
+  Iterator i_1 = begin_1;
+  Iterator i_2 = begin_2;
+
+  while( i_1 != end_1 && i_2 != end_2 )
+  {
+    if( *i_1 != *i_2 )
+    {
+      return false;
+    }
+    std::advance( i_1, 1 );
+    std::advance( i_2, 1 );
+  }
+
+  if( i_1 != end_1 || i_2 != end_2 )  // Different lengths
+  {
+    return false;
+  }
+
+  return true;
+}
+
 }  // Unnamed namespace for local functions
 
 int main()
 {
   srand(time(0));  // Seeding random number generator
-  std::vector<int> v1;
+  std::vector<int> v_unsorted;
   for( unsigned int i = 0; i < 10; ++i )
   {
-    v1.push_back( rand() % 100 );
+    v_unsorted.push_back( rand() % 100 );
   }
-  std::vector<int> v2( v1.begin(), v1.end() );
+  std::vector<int> v_sorted( v_unsorted.begin(), v_unsorted.end() );
+  std::vector<int> v_sorted_correct( v_unsorted.begin(), v_unsorted.end() );
+  std::sort( v_sorted_correct.begin(), v_sorted_correct.end() );
 
   std::cout << std::endl
             << "TESTING C++ SORTS:"
@@ -72,7 +108,7 @@ int main()
     switch( num )
     {
       case 1:
-        MergeSort<int>( v2.begin(), v2.end() );
+        MergeSort<int>( v_sorted.begin(), v_sorted.end() );
         invalid_input = false;
         break;
       default:
@@ -82,12 +118,28 @@ int main()
   }
 
   std::cout << "The contents of the vector before sorting are ";
-  PrintHorizontal( v1.begin(), v1.end() );
+  PrintHorizontal( v_unsorted.begin(), v_unsorted.end() );
   std::cout << std::endl;
 
   std::cout << "The contents of the vector after sorting are  ";
-  PrintHorizontal( v2.begin(), v2.end() );
+  PrintHorizontal( v_sorted.begin(), v_sorted.end() );
   std::cout << std::endl;
+
+  bool sorted_correctly = Equivalent( v_sorted.begin(),
+                                      v_sorted.end(),
+                                      v_sorted_correct.begin(),
+                                      v_sorted_correct.end() );
+  if( sorted_correctly )
+  {
+    std::cout << "The vector was successfully sorted." << std::endl;
+  }
+  else
+  {
+    std::cout << "The vector was not successfuly sorted; the correct "
+      << "vector should be ";
+    PrintHorizontal( v_sorted_correct.begin(), v_sorted_correct.end() );
+    std::cout << "." << std::endl;
+  }
 
   std::cout << std::endl;
   return 0;
