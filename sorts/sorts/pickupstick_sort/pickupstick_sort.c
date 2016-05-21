8642cfa73bc220d4edb557f83f0d431067fadef2
jleung51 <jleung513@gmail.com>
2016-05-20 23:06:46 -0700

    Sorts/PickupStick: Add empty method stub/header

---
+++ sorts/sorts/pickupstick_sort/pickupstick_sort.c
@@ -0,0 +1,18 @@
+/*
+ *
+ * This C program contains an implementation of a pickup-stick algorithm on an
+ * array of integers.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pickupstick_sort.h"
+
+// This function sorts an array from least to greatest using the
+// pickupstick sort algorithm.
+void pickupstick_sort( int* array, unsigned int len )
+{
+  return;
+}
---
+++ sorts/sorts/pickupstick_sort/pickupstick_sort.h
@@ -0,0 +1,15 @@
+/*
+ *
+ * This C program contains an implementation of a pickup-stick algorithm on an
+ * array of integers.
+ *
+ */
+
+#ifndef PICKUPSTICK_SORT_H
+#define PICKUPSTICK_SORT_H
+
+// This function sorts an array from least to greatest using the
+// pickupstick sort algorithm.
+void pickupstick_sort( int* array, unsigned int len );
+
+#endif
