494a60098a74ac0e50810767ecb39d543ee181e1
jleung51 <jleung513@gmail.com>
2016-05-20 23:11:27 -0700

    Sorts/PickupStick: Write function for sorted check

--- sorts/sorts/pickupstick_sort/pickupstick_sort.c
+++ sorts/sorts/pickupstick_sort/pickupstick_sort.c
@@ -10,6 +10,24 @@
 
 #include "pickupstick_sort.h"
 
+// This local function returns 1 if an array is sorted, and 0 otherwise.
+static int is_sorted( int* array, unsigned int len );
+
+// This local function returns 1 if an array is sorted, and 0 otherwise.
+static int is_sorted( int* array, unsigned int len )
+{
+  int i;
+  for( i = 0; i < len-1; ++i )
+  {
+    if( array[i] > array[i+1] )
+    {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
 // This function sorts an array from least to greatest using the
 // pickupstick sort algorithm.
 void pickupstick_sort( int* array, unsigned int len )
